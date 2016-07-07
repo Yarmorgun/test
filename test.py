6c97cd204d51eaf28307d683a6ae61fe261aa942
Yarmorgun <yarmorgun@gmail.com>
2016-07-07 20:42:07 +0300

    .

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 def openFile():
     for line in open("test.txt"):
          chunks = line.replace('%', '').replace('\n', '').split('\t')
-         print(chunks[2])
+         print(chunks[1])
 
 openFile()
